9f8f0e43ed7bb52782f736e942e944d41b38024d
gudmundurste12 <gudmundurste12@ru.is>
2014-09-25 11:24:53 +0000

    The tests can now be run

--- src/test/is/ru/honn/ruber/test/TestRuberService.java
+++ src/test/is/ru/honn/ruber/test/TestRuberService.java
@@ -6,12 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.logging.Logger;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration("classpath:app-test-stub.xml")
+@ContextConfiguration(locations={"/res/app-test-stub.xml"})
 public class TestRuberService extends TestCase
 {
 
