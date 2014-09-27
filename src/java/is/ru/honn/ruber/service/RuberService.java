07c2d4431c092e8047b155b1f15c8a94885944d1
gudmundurste12 <gudmundurste12@ru.is>
2014-09-27 10:43:55 +0000

    Moved the new methods in RuberService to a new interface that RuberService now extends

--- src/java/is/ru/honn/ruber/service/RuberService.java
+++ src/java/is/ru/honn/ruber/service/RuberService.java
@@ -7,19 +7,10 @@ import is.ru.honn.ruber.domain.User;
 
 import java.util.List;
 
-public interface RuberService
+public interface RuberService extends RuberUserService
 {
 	//Functions from assignment 1
 	public List<Product> getProducts(double latitude, double longitude) throws ServiceException;
 	public List<Price> getPriceEstimates(double startLatitude, double startLongitude,
 										 double endLatitude, double endLongitude) throws ServiceException;
-
-	//Functions from assignment 2
-	//TODO: Make sure the parameters and return values make sense
-	public void addTrips();
-	public History getHistory(User user);
-	public void signup(String userName, String firstName, String lastName, String password, String email, String picture, String promoCode) throws UsernameExistsException;
-	public List<User> getUsers();
-	public List<User> getUsers(int pageNumber) throws ArrayIndexOutOfBoundsException;
-	public User getUser() throws UserNotFoundException;
 }
